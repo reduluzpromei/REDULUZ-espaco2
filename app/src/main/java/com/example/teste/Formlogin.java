9a47eedc8a8956ff8549b358dbc2088d894d148c
reduluzpromei <reduluz.promei@gmail.com>
2022-06-14 13:37:23 -0300

    teste espaço 1

---
+++ app/src/main/java/com/example/teste/Formlogin.java
@@ -0,0 +1,14 @@
+package com.example.teste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Formlogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formlogin);
+    }
+}
